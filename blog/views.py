4d8a663c38dd2f03ea5b28621c37304e341adc04
mikolaz <mikolaz27@outlook.com>
2017-11-09 23:57:46 +0200

    add pagination

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
@@ -13,6 +14,18 @@ def post_list(request):
         posts = Post.objects.filter(title__icontains=query)
     else:
         posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+    paginator = Paginator(posts,3)
+
+
+    page=request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def calendar(request):
@@ -53,3 +66,4 @@ def post_edit(request,pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
